354e04f4037395eadf604ec7cfdd167a45b875a0
Suren Tekkem <suren.tekkem@acoustic.co>
2022-08-19 11:36:05 -0500

    added readme

--- cucumber/src/test/java/testRun/commonValues.java
+++ cucumber/src/test/java/testRun/commonValues.java
@@ -2,7 +2,7 @@ package test.java.testRun;
 
 public class commonValues {
     // token expires everytime code is checked into git. generate a new token and update variable when necessary
-    public static String token = "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";
+    public static String token = "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";
     public static String accept_git = "application/vnd.github+json";
     public static String contentType = "application/json";
     public static String baseUrl = "http://api.github.com";
