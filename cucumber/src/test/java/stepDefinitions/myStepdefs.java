d45b2daf2d716e17a52228f8334c478f395262e3
Suren Tekkem <suren.tekkem@acoustic.co>
2022-08-19 07:47:36 -0500

    clean up code

--- cucumber/src/test/java/stepDefinitions/myStepdefs.java
+++ cucumber/src/test/java/stepDefinitions/myStepdefs.java
@@ -1,7 +1,5 @@
 package test.java.stepDefinitions;
 
-import com.fasterxml.jackson.core.JsonParser;
-import groovyjarjarantlr4.v4.runtime.misc.Nullable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -9,19 +7,11 @@ import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import io.restassured.response.ResponseBody;
-import io.restassured.response.ResponseBodyData;
 import io.restassured.specification.RequestSpecification;
-import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.junit.Assert;
 import test.java.testRun.statusCode;
-
-import java.awt.print.Book;
 import java.util.List;
-import java.util.Optional;
-
 import static io.restassured.RestAssured.given;
 
 public class myStepdefs {
@@ -29,9 +19,6 @@ public class myStepdefs {
     RequestSpecification request;
     Response response;
 
-    ResponseBody body;
-
-    ResponseBodyData bodyData;
     @Given("^when user makes an api call using (.*)")
     public void whenUserTriesToGetRepositoriesFromURL(String url) {
        RestAssured.baseURI = url;
@@ -39,12 +26,10 @@ public class myStepdefs {
     @And("^passes authentication token with requests (.*)$")
     public void passesAuthenticationTokenWithRequestsToken(String token) {
         request = given().auth().oauth2(token).accept("application/vnd.github+json");
-        //request = given().accept("application/vnd.github+json");
     }
 
     @When("^call made with api path (.*)$")
     public void gitReturnsTheRepositories(String path) {
-        // String path = ;
         response = request.get(path).then().extract().response();
         String jsonStr = response.getBody().asString();
         System.out.println("payload: " + jsonStr);
@@ -53,16 +38,12 @@ public class myStepdefs {
     @Then("^validate the response code is (.*)$")
     public void validateTheResponseCodeIsCode(int code) {
         Assert.assertEquals(response.getStatusCode(),statusCode.sc_get);
-        //body = response.getBody();
     }
 
     @Then("^check a repo is returned in response (.*)$")
     public void checkARepoIsReturnedInResponseRepo(String repo) throws JSONException {
         Assert.assertEquals(true, response.getBody().asString().contains(repo));
-        String responseVal;
 
-
-        //System.out.println("****************** " + responseVal );
     }
 
     @Then("^verify value of (.*) is (.*) at (.*)$")
@@ -89,34 +70,7 @@ public class myStepdefs {
             System.out.println("no of occurrences of " + pKey + " in payload: " + values.size());
             keyVal = values.get(location);
 
-        //System.out.println(response.asString());
-        //JsonPath payload = new JsonPath(response.asString());
-        //keyVal=payload.getString(pKey);
-
         return keyVal;
-        /*
-        //get values of JSON array after getting array size
-        int s = j.getInt("owner.size()");
-        for(int i = 0; i < s; i++) {
-            String state = j.getString("login["+i+"]");
-            System.out.println(state);
-        }
-
-        JSONObject jsonResp = new JSONObject(jsonStr.toString());
-
-        // Get json array
-        JSONArray getArray = jsonResp.getJSONArray("owner");
-
-        // Get json object
-        JSONObject arrayItem = getArray.getJSONObject(0);
-
-        // Print and display commands
-        System.out.println(
-                String.format("login - %s", arrayItem.get("login")));
-
-        // return sampleJson;
-
-         */
     }
 
     @Then("^verify key (.*) has value (.*) using jsonpath$")
